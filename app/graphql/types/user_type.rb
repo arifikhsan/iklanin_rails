5d028e48fc419526ef85dca933a6a82e9ec9d651
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-23 10:06:53 +0700

    user detail type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, Integer, null: false
     field :email, String, null: false
-    field :role, Integer, null: true
+    field :role, Integer, null: false
+    field :user_Detail, UserDetailType, null: false
   end
 end
