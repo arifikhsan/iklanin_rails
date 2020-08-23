65824ec4d4222a89d653a947995aff842bde2daf
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-23 15:16:59 +0700

    test

--- test/models/ad_test.rb
+++ test/models/ad_test.rb
@@ -1,17 +1,7 @@
 require 'test_helper'
 
 class AdTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
-  # test "the false" do
-  #   assert_not false
-  # end
-
-  # test "should not save without anything" do
-    # binding.pry
-    # assert_not Ad.new.save
-    # assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
