96bf85a98d0403d71c854d99023f54ad2fe52307
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-13 12:22:45 +0700

    eagerload user detail

--- app/controllers/api/v1/ads_controller.rb
+++ app/controllers/api/v1/ads_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AdsController < Api::BaseController
   before_action :set_ad, only: [:update, :show, :destroy]
 
   def index
-    @ads = Ad.active.page(params[:page]).includes(:category, :user, ad_images: {image_attachment: :blob})
+    @ads = Ad.active.page(params[:page]).includes(:category, user: :user_detail, ad_images: {image_attachment: :blob})
   end
 
   def show
