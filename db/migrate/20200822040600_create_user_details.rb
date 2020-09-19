211b21eb2e00dc4cbe68422cf43311d4d5cedc0e
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-19 09:49:09 +0700

    user detail

--- db/migrate/20200822040600_create_user_details.rb
+++ db/migrate/20200822040600_create_user_details.rb
@@ -3,6 +3,16 @@ class CreateUserDetails < ActiveRecord::Migration[6.0]
     create_table :user_details do |t|
       t.references :user, null: false, foreign_key: true
       t.string :name
+      t.string :phone_number
+      t.string :whatsapp_phone_number
+      t.string :address
+      t.string :village
+      t.string :district
+      t.string :regency
+      t.string :province
+      t.string :zip_code
+      t.date   :dob
+      t.integer :gender
 
       t.timestamps
     end
